7fea105f51f0448bce8cd9a13ca9269dda6e160c
Ayaan Siddiqui <ayaanqui@pop-os.localdomain>
2020-06-01 19:16:00 -0500

    Initial commit

---
+++ Autocorrect.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.IntStream;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class Autocorrect {
+    private final char[] delimeters = new char[] { ' ', ',', '.', '(', ')', '"', '\'', '“', '”', '’', '!', ';' };
+    private char[] alphabets = new char[26];
+
+    private String text; // Holds the original text of the user
+    private ArrayList<String> parsedText;
+    private HashMap<String, String> wordsMap;
+
+    public Autocorrect() throws IOException {
+        wordsMap = new HashMap<>();
+        // Load words into wordMap
+        loadWords(new String[] { "words_alpha.txt", "words.txt", "words_english.txt" });
+        // Initialize alphabets
+        IntStream.range(0, 26).forEach(i -> alphabets[i] = (char) ('a' + i));
+    }
+
+    public void suggest(String text) throws IOException {
+        this.text = text;
+        parse();
+
+        // Search for words
+        search();
+    }
+
+    private void loadWords(String[] wordsArray) throws IOException {
+        for (String wordFileName : wordsArray) {
+            Scanner file = new Scanner(new File(wordFileName));
+            while (file.hasNext()) {
+                String word = file.next().toLowerCase();
+                wordsMap.put(word, word);
+            }
+            file.close();
+        }
+    }
+
+    /**
+     * Input: "Hello, this is Ayaan" <br/>
+     * Output: ["Hello", ",", "this", "is", "Ayaan"]
+     */
+    private void parse() {
+        // Reset ArrayList
+        parsedText = new ArrayList<>();
+
+        int start = 0;
+        for (int i = 0; i < text.length(); i++) {
+
+            for (char delim : delimeters) {
+                if (text.charAt(i) == delim) {
+                    String sub = text.substring(start, i);
+                    if (sub.length() > 0)
+                        parsedText.add(sub);
+                    start = i + 1;
+                    break;
+                }
+            }
+        }
+
+        String remainder = text.substring(start);
+        if (!remainder.equals(""))
+            parsedText.add(remainder);
+    }
+
+    private void search() {
+        ArrayList<String> invalidWords = new ArrayList<>();
+        parsedText.forEach(word -> {
+            if (wordsMap.get(word.toLowerCase()) == null)
+                invalidWords.add(word);
+        });
+
+        System.out.println("Invalid list of words: " + invalidWords);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Autocorrect ob = new Autocorrect();
+
+        String input = "";
+        Scanner keyboard = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter phrase (type -1 to quit): ");
+            input = keyboard.nextLine();
+
+            if (input.equals("-1"))
+                break;
+            else {
+                ob.suggest(input);
+                System.out.println("\n");
+            }
+        }
+        keyboard.close();
+    }
+}
